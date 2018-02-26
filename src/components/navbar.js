a2f28617a313f13513b990be56673d1664dd18fc
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-26 08:22:25 +0700

    Change navbar elements & remove unused method handler

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,32 +3,22 @@ import React, { Component } from 'react';
 import './navbar.css';
 
 class Navbar extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			resize: ''
-		}
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-	handleClick() {
-		const linksEl = document.getElementById('myNavbar');
-		if (linksEl.className.trim() === 'navbar') {
-			this.setState({ resize: 'responsive' });
-		} else {
-			this.setState({ resize: '' });
-		}
-	}
-
 	render() {
 		return(
-			<div className={`navbar ${this.state.resize}`} id="myNavbar">
-			  <a href="#home" className="active">Home</a>
-			  <a href="#news">News</a>
-			  <a href="#contact">Contact</a>
-			  <a href="#about" style={{ float: 'right' }}>About</a>
-			  <a href="#" style={{fontSize: '15px'}} className="icon" onClick={this.handleClick}>&#9776;</a>
-			</div>
+			<nav className="navbar navbar-default navbar-fixed-bottom">
+	      <div className="container-fluid">
+          <ul className="navbar-center">
+					 	<li><a href="/search"><i className="fa fa-plus-square-o fa-3x" aria-hidden="true"></i> </a><p>Search</p></li>
+					 	<li><a href="/games"><i className="fa fa-gamepad fa-3x" aria-hidden="true"></i> </a><p>Games</p></li>
+					 	<li><a href="/shop"><i className="fa fa-shopping-bag fa-3x" aria-hidden="true"></i> </a><p>Shop</p></li>
+					 	<li><a href="/friends"><i className="fa fa-address-book-o fa-3x" aria-hidden="true"></i> </a><p>Friends</p></li>
+					 	<li><a href="/messages"><i className="fa fa-commenting-o fa-3x" aria-hidden="true"></i> </a><p>Messages</p></li>
+				    <li><a href="achievements"><i className="fa fa-trophy fa-3x" aria-hidden="true"></i> </a><p>Achievements</p></li>
+				    <li><a href="/settings"><i className="fa fa-cog fa-3x" aria-hidden="true"></i> </a><p>Settings</p></li>
+				    <li><a href="/turnoff"><i className="fa fa-power-off fa-3x" aria-hidden="true"></i> </a><p>Turn Off</p></li>
+				  </ul>
+        </div>
+	    </nav>
 		);
 	}
 }
