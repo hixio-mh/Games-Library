cb513c55e0d1fdc89af41faca759e7fc0fc30082
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-27 08:32:54 +0700

    Chnge games url

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import ImageSlider from './components/images_slider';
 import Navbar from './components/navbar';
 
 const GAMES = [
-  { title: 'Tomb Raider', imageUrl: 'https://i.imgur.com/eOtEAB7.jpg' },
-  { title: 'Call of Duty Black Ops III', imageUrl: 'https://i.kinja-img.com/gawker-media/image/upload/t_original/b5adrhpqn58d1pijxkc1.jpg' },
+  { title: 'Tomb Raider', imageUrl: 'http://www.oxpal.com/wp-content/uploads/2014/08/tomb_raider_survivor_born_-_cover.jpg' },
+  { title: 'Call of Duty Black Ops II', imageUrl: 'http://www.oxpal.com/wp-content/uploads/2014/08/call_of_duty_black_ops_2_-_cover.jpg' },
   { title: 'Far Cry 3', imageUrl: 'http://www.oxpal.com/wp-content/uploads/2014/08/far_cry_3_-_cover.jpg' },
   { title: 'Hitman - Absolution', imageUrl: 'http://www.oxpal.com/wp-content/uploads/2014/08/hitman_absolution_-_cover.jpg' },
   { title: 'Uncharted 2', imageUrl: 'http://www.oxpal.com/wp-content/uploads/2014/08/uncharted_2_-_cover.jpg' },
