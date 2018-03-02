a136f0da55f65c95398c043071a5c0c3f164ce05
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-03-02 15:00:17 +0700

    Add conditional rendering for navbar center menus, according to the main menu event

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,6 +3,20 @@ import Clock from 'react-live-clock';
 
 import './navbar.css';
 
+const MenuCenter = () => {
+	return(
+		<React.Fragment>
+			<li><a href="/search"><i className="fa fa-plus-square-o fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Search</p></li>
+			<li><a href="/games"><i className="fa fa-gamepad fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Games</p></li>
+			<li><a href="/shop"><i className="fa fa-shopping-bag fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Shop</p></li>
+			<li><a href="/friends"><i className="fa fa-address-book-o fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Friends</p></li>
+			<li><a href="/messages"><i className="fa fa-commenting-o fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Messages</p></li>
+			<li><a href="achievements"><i className="fa fa-trophy fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Achievements</p></li>
+			<li><a href="/settings"><i className="fa fa-cog fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Settings</p></li>
+			<li><a href="/turnoff"><i className="fa fa-power-off fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Turn Off</p></li>
+		</React.Fragment>
+	);
+}
 class Navbar extends Component {
 	render() {
 		return(
@@ -10,17 +24,11 @@ class Navbar extends Component {
 	      <div className="container-fluid">
           <ul className="navbar-center">
 					 	<li className="pull-left"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/590489/profile/profile-512.jpg" className="image-cover" alt="Profile"/></li>
-					 	<li className="pull-left"><p className="desc-text">Nubilfi</p></li>
-
-					 	<li><a href="/search"><i className="fa fa-plus-square-o fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Search</p></li>
-					 	<li><a href="/games"><i className="fa fa-gamepad fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Games</p></li>
-					 	<li><a href="/shop"><i className="fa fa-shopping-bag fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Shop</p></li>
-					 	<li><a href="/friends"><i className="fa fa-address-book-o fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Friends</p></li>
-					 	<li><a href="/messages"><i className="fa fa-commenting-o fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Messages</p></li>
-				    <li><a href="achievements"><i className="fa fa-trophy fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Achievements</p></li>
-				    <li><a href="/settings"><i className="fa fa-cog fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Settings</p></li>
-				    <li><a href="/turnoff"><i className="fa fa-power-off fa-3x" aria-hidden="true"></i> </a><p className="menu-title">Turn Off</p></li>
-				    
+					 	<li className="pull-left"><p className="desc-text">Habil</p></li>
+					 	{
+					 		this.props.isMenuClicked &&
+					 		<MenuCenter />
+					 	}				    
 				    <li className="pull-right"><p className="desc-text"><Clock format={'HH:mm'} timezone={'Asia/Jakarta'}/></p></li>
 				    <li className="pull-right"><i className="fa fa-signal fa-2x" aria-hidden="true"></i></li>
 				  </ul>
