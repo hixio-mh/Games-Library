7abc6b1072db8b92b8fa1775d84ed05ef0f8282d
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-05-29 12:59:07 +0700

    Update games collection component css & add some methods to the component

--- src/components/games_collection.js
+++ src/components/games_collection.js
@@ -1,54 +1,66 @@
 import React, { Component } from 'react';
 import Slider from 'react-slick';
 
+import Navbar from './navbar';
 import './games_collection.css';
 
 class GamesCollection extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			slideOpts: {
-	      autoplay: true,
-	      autoplaySpeed: 6000,
-	      centerMode: true,
-	      dots: false,
-	      focusOnSelect: false,
-	      infinite: true,
-	      initialSlide: 0,
-	      responsive: [
-	      	{ breakpoint: 768, settings: { slidesToShow: 2 } },
-	      	{ breakpoint: 1024, settings: { slidesToShow: 4 } },
-	      	{ breakpoint: 1366, settings: { slidesToShow: 6 } },
-	      	{ breakpoint: 10000, settings: { slidesToShow: 6 } }
-      	],
-      	slidesToScroll: 1,
-	      speed: 500,
-			}
-		};
-	}
+    this.state = {
+      slideOpts: {
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        dots: false,
+        focusOnSelect: false,
+        infinite: true,
+        initialSlide: 0,
+        responsive: [
+          { breakpoint: 768, settings: { slidesToShow: 2 } },
+          { breakpoint: 1024, settings: { slidesToShow: 4 } },
+          { breakpoint: 1366, settings: { slidesToShow: 6 } },
+          { breakpoint: 10000, settings: { slidesToShow: 6 } },
+        ],
+        slidesToScroll: 1,
+        speed: 500,
+      },
+    };
+  }
 
-	render() {
-		const { games } = this.props;
-		return(
-			<div className="row">
-				<Slider {...this.state.slideOpts}>
-				{
-					games.map((data) => {
-					  return(
-					  	<div className="game-cover" key={data.title}>
-					  		<a href="/check">
-						  		<img src={data.imageUrl} style={{ width: '100%' }} alt={data.title}/>
-					  		</a>
-						    <p className="game-title">{data.title}</p>
-						  </div>
-					  )
-					})
-				}
-				</Slider>
-			</div>
-		);
-	}
+  componentDidMount() {
+    this.props.onHandleMenuClick();
+  }
+  componentWillUnmount() {
+    this.props.onHandleMenuClick();
+  }
+
+  render() {
+    const { games, onMenuClick } = this.props;
+
+    return (
+      <React.Fragment>
+        <div className="row-slider">
+          <Slider {...this.state.slideOpts}>
+            {
+              games.map((data) => {
+                return (
+                  <div className="game-cover fade-in" key={data.title}>
+                    <a href="/check">
+                      <img src={data.imageUrl} style={{ width: '100%' }} alt={data.title} />
+                    </a>
+                    <p className="game-title">{data.title}</p>
+                  </div>
+                );
+              })
+            }
+          </Slider>
+        </div>
+        <Navbar isMenuClicked={onMenuClick} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default GamesCollection;
